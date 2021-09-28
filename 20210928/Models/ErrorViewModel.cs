aa03438dfdb393086b76371e0b487866fe3d0f4f
Галанин Дмитрий Николаевич <DmiNGalanin@int.kpfu.ru>
2021-09-28 17:51:56 +0300

    Добавьте файлы проекта.

---
+++ 20210928/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _20210928.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
